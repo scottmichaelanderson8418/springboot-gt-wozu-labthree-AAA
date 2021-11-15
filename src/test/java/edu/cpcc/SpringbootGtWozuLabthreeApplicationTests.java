0d2ec76a4b5482201bea96f2eecbc21ff4ecd7f4
SCOTT <scottmichaelanderson8418@gmail.com>
2021-11-15 13:05:13 -0500

    Initial Release...

---
+++ src/test/java/edu/cpcc/SpringbootGtWozuLabthreeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGtWozuLabthreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
